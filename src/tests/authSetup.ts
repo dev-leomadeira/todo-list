475d8de1adfb79ba3f5e2212b8305108bb029d31
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-04 22:01:08 -0300

    Create authSetup.ts

---
+++ src/tests/authSetup.ts
@@ -0,0 +1,30 @@
+import request from 'supertest';
+import express from 'express';
+import bodyParser from 'body-parser';
+import authRouter from '../routes/authRoutes';
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/auth', authRouter);
+
+let token: string;
+
+const getToken = async () => {
+  if (!token) {
+    const loginResponse = await request(app)
+      .post('/auth/logar')
+      .send({ 
+        email: 'admin@gmail.com', 
+        senha: 'root' 
+      });
+
+    if (loginResponse.status === 200) {
+      token = loginResponse.body.token;
+    } else {
+      throw new Error('Erro ao realizar login');
+    }
+  }
+  return token;
+};
+
+export { getToken };
