2dddee8ed45d2a369d80d9d9a84c61694d9d3958
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-23 05:59:44 -0300

    Routes - complet

--- src/routes/tarefaRouter.ts
+++ src/routes/tarefaRouter.ts
@@ -5,8 +5,9 @@ import { authMiddleware } from "../middleware/authMiddleware";
 const router = Router();
 
 router.post("/tarefas", authMiddleware, TarefaController.criarTarefa);
-router.get("/tarefas/lista/:listaId", authMiddleware, TarefaController.buscarTarefasPorLista);
+router.get("/tarefas", authMiddleware, TarefaController.buscarTodosTarefas);
+router.get("/tarefas/:id", authMiddleware, TarefaController.buscarTarefaPorId);
 router.put("/tarefas/:id", authMiddleware, TarefaController.atualizarTarefa);
-router.delete("/tarefas/:id/lista/:listaId", authMiddleware, TarefaController.deletarTarefa);
+router.delete("/tarefas/:id", authMiddleware, TarefaController.deletarTarefa);
 
 export default router;
