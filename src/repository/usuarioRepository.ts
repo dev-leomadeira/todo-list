2e37d83af02115f7a37054128c41227df86102d5
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-23 05:59:24 -0300

    repository update - bugs

--- src/repository/usuarioRepository.ts
+++ src/repository/usuarioRepository.ts
@@ -2,28 +2,33 @@ import Usuario from "../models/usuario.model";
 import { UsuarioAtributos, UsuarioAtributosCriacao } from "../interface/usuario.interface";
 
 class UsuarioRepository {
-    public async criarUsuario(usuario: UsuarioAtributosCriacao): Promise<Usuario> {
+    async criarUsuario(usuario: UsuarioAtributosCriacao): Promise<Usuario> {
         return Usuario.create(usuario);
     }
 
-    public async buscarUsuarioPorId(id: number): Promise<Usuario | null> {
+    async buscarUsuarioPorId(id: number): Promise<Usuario | null> {
         return Usuario.findByPk(id);
     }
 
-    public async buscarTodosUsuarios(): Promise<Usuario[]> {
+    async buscarTodosUsuarios(): Promise<Usuario[]> {
         return Usuario.findAll();
     }
 
-    public async atualizarUsuario(id: number, dadosAtualizados: Partial<UsuarioAtributos>): Promise<[number, Usuario[]]> {
-        return Usuario.update(dadosAtualizados, {
-            where: { id },
-            returning: true
-        });
+    async atualizarUsuario(id: number, dadosAtualizados: Partial<UsuarioAtributos>): Promise<Usuario> {
+        const existeUsuario = await this.buscarUsuarioPorId(id);
+        if (!existeUsuario) {
+            throw new Error("Usário não encontrado.");
+        }
+        return await existeUsuario.update(dadosAtualizados);
     }
 
-    public async deletarUsuario(id: number): Promise<number> {
-        return Usuario.destroy({ where: { id } });
+    async deletarUsuario(id: number): Promise<void> {
+        const existeUsuario = await this.buscarUsuarioPorId(id);
+        if (!existeUsuario) {
+            throw new Error(`Usuario com o id ${id} não encontrado.`);
+        }
+        await existeUsuario.destroy();
     }
 }
 
-export default UsuarioRepository;
+export default new UsuarioRepository();
